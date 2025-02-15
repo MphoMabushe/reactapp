c73a1d8fc0e9ad07f4072db65a61fb7ac38a4bea
MphoMabushe <wendyxmabushe@gmail.com>
2025-02-15 19:47:01 +0200

    created a react weather app

---
+++ src/weather.js
@@ -0,0 +1,45 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather(props){
+    function handleResponse(response){
+        alert(`The weather in ${response.data.name} is ${response.data.main.temp}℃ `)
+    }
+    let apiKey="7054937c48b3e61756fdcf53090c10fa";
+    let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+    
+    axios.get(apiUrl).then(handleResponse);
+return(
+    <div id="root">
+      <div className="weather-contents">
+      <header>
+       <form class="search-form" id="search-form">
+              <input type="search" placeholder="Enter a city" required id="search-box" class="search-box"/>
+              <input type="submit" value="search" class="search-button"/>
+          </form> 
+          </header>
+      <main>
+          <div className="weather-details">
+      <h1 class="city-input" id="city"></h1>
+      <p class="weather-features"><span id="time"></span>,<span id="weather-type">clear sky</span>
+       <br/>
+       Humidity:<strong id="humidity"></strong>,Wind:<strong id="speed"></strong>
+      </p>
+      </div>
+      <div>
+      <div className="temperature-details">
+      <div id="emoji"></div>
+      <div className="temperature-number" id="temperature"></div>
+      <div className="temperature-degrees">°C</div>
+      </div>
+      </div>
+      </main>
+      <div className="weather-forecast" id="forecast">
+      <footer>This project was coded by <a href="https://github.com/MphoMabushe">Mpho Mabushe</a>,is <a href="https://github.com/MphoMabushe">open-souced on
+  Github</a> and <a href="https://mphoweatherapp.netlify.app/">hosted on Netlify</a>
+      </footer>
+  </div> 
+  </div>
+  </div>
+);
+}
