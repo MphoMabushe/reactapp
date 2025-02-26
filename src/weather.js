f36fbe1407d8e132f76c5ec4c8471d0995faea79
MphoMabushe <wendyxmabushe@gmail.com>
2025-02-26 19:03:00 +0200

    edited footer links

--- src/weather.js
+++ src/weather.js
@@ -37,8 +37,8 @@ return(
       </div>
       </main>
       <div className="weather-forecast" id="forecast">
-      <footer>This project was coded by <a href="https://github.com/MphoMabushe">Mpho Mabushe</a>,is <a href="https://github.com/MphoMabushe">open-souced on
-  Github</a> and <a href="https://mphoweatherapp.netlify.app/">hosted on Netlify</a>
+      <footer>This project was coded by <a href ="https://mpho-portfolio-d9f5d1.netlify.app/?Message=+++++++++++++++++m+++++++++++++++++++#mpho-projects" target="_blank" rel="noreferrer" >Mpho Mabushe </a>,is <a href="https://github.com/MphoMabushe/reactapp" target="_blank" rel="noreferrer">open-souced on
+  Github</a> and <a href="https://mphoweatherapp.netlify.app/" target="_blank" rel="noreferrer">hosted on Netlify</a>
       </footer>
   </div> 
   </div>
