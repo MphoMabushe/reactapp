a3af5f16c0b241fe0bb6fe900e9126b41b29d66e
MphoMabushe <wendyxmabushe@gmail.com>
2025-06-26 20:28:50 +0200

    added option for farenheit temperature unit

--- src/weathertemperature.js
+++ src/weathertemperature.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherTemperature(props){
+const [unit,setUnit]=useState("celsius")
+   function showFarenheit(event) {
+    event.preventDefault();
+   setUnit("farenheit");}
+   function showCelsius(event) {
+    event.preventDefault();
+   setUnit("celsius");
+   }
+if (unit === "celsius"){
 return(
     <div className="weathertemperature">
     <span className="temperature">
                     {Math.round(props.celsius)}
                     </span>
-                    <span className="unit">℃</span>
+                    <span className="unit">℃| {""} <a href ="/" className="text-decoration-none" onClick={showFarenheit}>F</a></span>
+
                 </div>
-);
+);}
+else{
+    let farenheit = (props.celsius * 9)/5 + 35
+    return(
+    <div className="weathertemperature">
+    <span className="temperature">
+                    {Math.round(farenheit)}
+                    </span>
+                    <span className="unit"> <a href="/" className="text-decoration-none"  onClick={showCelsius}>℃</a> {""} |F</span>
+
+                </div>);
+
+;
 }
+   }
